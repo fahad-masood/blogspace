930c16f0cc5134b1639dbfc96e68846e906891d2
Fahad <fahadmasood03@gmail.com>
2022-08-08 23:20:35 +0530

    refactor code

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+let postsArray = [];
+const titleInput = document.getElementById("post-title");
+const bodyInput = document.getElementById("post-body");
+const form = document.getElementById("new-post");
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArray) {
+    html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
-    const postsArr = data.slice(0, 5);
-    let html = "";
-    for (let post of postsArr) {
-      html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `;
-    }
-    document.getElementById("blog-list").innerHTML = html;
+    postsArray = data.slice(0, 5);
+    renderPosts();
   });
 
-document.getElementById("new-post").addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const postTitle = document.getElementById("post-title").value;
-  const postBody = document.getElementById("post-body").value;
+  const postTitle = titleInput.value;
+  const postBody = bodyInput.value;
   const data = {
     title: postTitle,
     body: postBody,
@@ -33,11 +42,8 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
   fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
     .then((res) => res.json())
     .then((post) => {
-      document.getElementById("blog-list").innerHTML = `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr /> 
-                ${document.getElementById("blog-list").innerHTML}
-            `;
+      postsArray.unshift(post);
+      renderPosts();
+      form.reset();
     });
 });
